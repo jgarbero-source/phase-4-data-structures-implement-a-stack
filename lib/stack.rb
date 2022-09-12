0df5fee8a969628a6ef3f779b8b047c3fbce7e7f
John Garbero <john.garbero.itb@gmail.com>
2022-09-12 10:49:37 -0500

    yasd

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,36 @@
 # your code here
+class Stack
+
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+    
+    def push(value)
+        @stack.push(value)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.peek
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        @limit && @stack.size == @limit
+    end
+
+    def search(value)
+        
+    end
+end
